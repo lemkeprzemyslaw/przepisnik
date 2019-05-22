28e6f6e9e5e1dcc862218ca86a14258cd5ae7250
lemkeprzemyslaw <lemkeprzemyslaw@gmail.com>
2019-05-22 21:31:08 +0200

    remove unused code

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,16 +3,8 @@ import { Link } from "react-router-dom";
 
 import * as ROUTES from '../constants/routes'
 
-import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
