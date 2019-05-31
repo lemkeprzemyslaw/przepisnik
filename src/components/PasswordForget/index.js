e5efbc5ce3637df7092e9e61ccf58dd5f937d53f
lemkeprzemyslaw <lemkeprzemyslaw@gmail.com>
2019-05-31 20:04:25 +0200

    add password forget feature

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
+
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes'
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,6 +15,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
+import CustomizedSnackbar from "../Snackbars";
 
 const styles = theme => ({
   main: {
@@ -43,41 +49,103 @@ const styles = theme => ({
   },
 });
 
-function PasswordForget(props) {
-  const {classes} = props;
+const PasswordForget = () => (
+  <div>
+    <PasswordForgetForm />
+  </div>
+);
 
-  return (
-    <main className={classes.main}>
-      <CssBaseline/>
-      <Paper className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon/>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Przypomnij hasło
-        </Typography>
-        <form className={classes.form}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Adres email</InputLabel>
-            <Input id="email" name="email" autoComplete="email" autoFocus/>
-          </FormControl>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Wyślij przypomnienie
-          </Button>
-        </form>
-      </Paper>
-    </main>
-  );
+const INITIAL_STATE = {
+  email: '',
+  error: null,
+};
+
+class PasswordForgetFormBase extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  };
+
+  onSubmit = event => {
+    const { email, error } = this.state;
+
+    this.props.firebase
+      .doPasswordReset(email)
+      .then(() => {
+        this.setState({ INITIAL_STATE });
+      })
+      .catch(() => {
+        this.setState({ error })
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, error } = this.state;
+    const isInvalid = email.trim() === '';
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline/>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon/>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Przypomnij hasło
+          </Typography>
+          <form className={classes.form} onSubmit={this.onSubmit}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Adres email</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+                type="text"
+                autoComplete="email"
+                autoFocus/>
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              disabled={isInvalid}
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Wyślij przypomnienie
+            </Button>
+          </form>
+        </Paper>
+        {error && <CustomizedSnackbar
+          variant='error'
+          message={error.message}
+        />}
+      </main>
+    );
+  }
 }
+
+const PasswordForgetLink = () => (
+  <Typography align='center'>
+    Nie pamiętasz hasła? <Link to={ROUTES.PASSWORD_FORGET}>Przypomnij hasło</Link>
+  </Typography>
+);
 
 PasswordForget.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(PasswordForget);
+
+const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
+export { PasswordForgetForm, PasswordForgetLink }
